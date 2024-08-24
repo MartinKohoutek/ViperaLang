ae34d11116d3ff7224fc9613ca74055ac5194e73
Martin Kohoutek <martinkohoutek@hotmail.com>
2024-08-24 10:32:33 +0200

    fix: add missing return to string() function

--- ViperaLang/src/com/martinkohoutek/vipera/Scanner.java
+++ ViperaLang/src/com/martinkohoutek/vipera/Scanner.java
@@ -92,6 +92,7 @@ public class Scanner {
 
         if (isAtEnd()) {
             Vipera.error(line, "Unterminated string.");
+            return;
         }
 
         advance();         // The closing ".
